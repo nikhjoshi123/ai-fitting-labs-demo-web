fe51f020ce8735893b7eec7c7b501a50bdb8dc0e
nikhjoshi123 <nhjoshi436@gmail.com>
2026-02-17 22:39:15 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,8 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/Home";
-import ProductPage from "@/pages/ProductPage";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/product/:handle" component={ProductPage} />
-      <Route component={NotFound} />
-    </Switch>
-  );
-}
-
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <TooltipProvider>
-        <Router />
-        <Toaster />
-      </TooltipProvider>
-    </QueryClientProvider>
-  );
-}
-
-export default App;
+import { Toaster } from "./components/ui/toaster"; // Fixed path
+import { TooltipProvider } from "./components/ui/tooltip"; // Fixed path
+import NotFound from "./pages/not-found"; // Fixed path
+import Home from "./pages/Home"; // Fixed path
+import ProductPage from "./pages/ProductPage"; // Fixed path
